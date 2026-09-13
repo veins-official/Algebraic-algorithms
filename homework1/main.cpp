4a7be1d08277fb120d4ce9b42c7ebe16d9fd1f98
Ilya <veins-official@mail.ru>
2026-09-13 21:47:41 +0300

    add lil test

--- homework1/main.cpp
+++ homework1/main.cpp
@@ -3,14 +3,22 @@
 
 
 int main (void) {
-  Vector v1;
+  Vector v1, v2;
 
   for (size_t i = 0; i < 10; i++) {
     v1.push(i);
   }
   
+  v2 = v1;
+  
+  v1.push(13);
+  
   for (size_t i = 0; i < v1.getSize(); i++) {
     std::cout << v1.getElement(i) << std::endl;
+  }
+  std::cout << std::endl;
+  for (size_t i = 0; i < v2.getSize(); i++) {
+    std::cout << v2.getElement(i) << std::endl; // Не совпадает с v1
   }
 
   return 0;
